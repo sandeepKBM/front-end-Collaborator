f0979ef376aa0d5263fd41aca509b3fe5c8f6320
sandeepKBM <67870452+sandeepKBM@users.noreply.github.com>
2022-09-10 16:40:32 +0530

    formatting directory

--- back_end/security/src/main/java/com/learn/security/repo/roleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.learn.security.repo;
-
-import com.learn.security.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface roleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- back_end/security/src/main/java/com/learn/security/repo/userRepo.java
+++
@@ -1,9 +0,0 @@
-package com.learn.security.repo;
-
-import com.learn.security.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userRepo extends JpaRepository<User,Long> {
-    User findByUsername(String username);
-
-}
