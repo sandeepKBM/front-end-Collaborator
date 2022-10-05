425231a7ea524a07ee79f2aa5c29c7b84ae191b4
r.navaneeth992 <r.navaneeth992@gmail.com>
2022-10-05 09:00:22 +0530

    Update forum.js

--- front-end/src/forum.js
+++ front-end/src/forum.js
@@ -3,62 +3,22 @@ import {useNavigate} from 'react-router-dom';
 import {useState,useEffect} from 'react';
 import Axios from "axios";
 function Forum() {
-  // const navigate=useNavigate();
-  // const [forum,setForum]=useState([{name:"Name",creation_on:"Creation ON",description:"description",created_by:"Created by"},
-  // {name:"Name",creation_on:"Creation ON",description:"description",created_by:"Created by"}]);
-  // const [quotes,setQuotes]=useState("");
-  //   const [desc,setDesc]=useState("");
-  //   function SubmitForum(){
-  //       if(quotes==='' && desc==='')
-  //       {
-  //           let message="";
-  //           if(quotes==='')
-  //           {
-  //               message+="Please enter quotes\n";
-  //           }
-  //           if(desc==='')
-  //           {
-  //               message+="Please enter desc\n";
-  //           }
-  //           alert(message);
-  //       }
-  //       else{
-  //           alert('sucessfully done');
-  //           setForum([...forum,{name:quotes,creation_on:"Creation ON",description:desc,created_by:"Created by"}])
-  //       }
-  //   }
   const navigate=useNavigate();
-    // let forum = [{name:"Name",creation_on:"Creation ON",description:"description",created_by:"Created by"}]
-    const [forum, setState] = useState([])
-    useEffect(() => {
-        getData()
-    },[])
-    async function getData(){
-        Axios.get('http://localhost:8080/api/discussions/listAll').then((res) => {
-        setState(res.data)
-        });
-    }
+  const [forum, setState] = useState([{name:"Name",creation_on:"Creation ON",description:"description",created_by:"Created by"}])
+  useEffect(() => {
+      getData()
+  },[])
+  async function getData(){
+      Axios.get('http://localhost:8080/api/discussions/listAll').then((res) => {
+      setState(res.data)
+      });
+  }
   return (
     <div className="App">
-      {/* <button id="add-icon" onClick={()=>{navigate('/adddiscussion')}}>Create</button> */}
       <br></br>
-      {/* <div className="Login">
-        <center>
-          <h2>Add Discussions</h2>
-            <div id="login-basic">
-              <label>Name :  </label><input type="text"
-              onChange={(event)=>{setQuotes(event.target.value)}}></input>
-              <br></br>
-              <br></br>
-              <label>Description :  </label><input type="text"
-              onChange={(event)=>{setDesc(event.target.value)}}></input>
-              <br></br>
-              <br></br>
-              <button onClick={SubmitForum}>Submit</button>
-            </div>
-            <br></br>
-        </center>
-    </div> */}
+      <div>
+        
+      </div>
         {
           forum.map((val)=>{
               return<>
@@ -91,5 +51,4 @@ function Forum() {
     </div>
   );
 }
-
 export default Forum;
