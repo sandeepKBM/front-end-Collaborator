b6f606a7196120f7b6bb38980479d976091d8b4f
r.navaneeth992 <r.navaneeth992@gmail.com>
2023-02-13 15:27:20 +0530

    added text submit for discussion

--- front-end/src/dispage.js
+++ front-end/src/dispage.js
@@ -1,10 +1,35 @@
 import './App.css';
 import  {useState ,useEffect} from "react";
 import Axios from "axios";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 let vals = ""
 function DisPage() {
     const [quotes,setQuotes]=useState("");
-    const [textarr,setTextArr]=useState([])
+    const [textarr,setTextArr]=useState([]);
+    const [open, setOpen] = useState(false);
+    const [textBox,setTextBox]=useState("");
+    const [id,setID]=useState("");
+    const handleClickOpen = (id) => {
+      setID(id);
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      let objtemp=textarr;
+      let objIndex = objtemp.findIndex((obj => obj.id == id));
+      objtemp[objIndex].adddisc.push(
+        {text:textBox,time: new Date().toLocaleString()}
+      );
+      setTextArr(objtemp);
+      // setTextArr([...textarr,{text:quotes,id: Math.trunc(Math.random()*1000000000000000), time: new Date().toLocaleString(),adddisc:[] }]);
+      setOpen(false);
+    };
     useEffect(() => {
         getData()
     },[])
@@ -31,7 +56,7 @@ function DisPage() {
         alert(message);
     }
     else{
-      setTextArr([...textarr,{text:quotes,id: 200, time: new Date().toLocaleString() }]);
+      setTextArr([...textarr,{text:quotes,id: Math.trunc(Math.random()*1000000000000000), time: new Date().toLocaleString(),adddisc:[] }]);
       let value1 = localStorage.getItem("id");
       let value2 = localStorage.getItem("dissID");
       console.log(typeof vals)
@@ -55,11 +80,35 @@ function DisPage() {
                 {val.text}
               </div>
               <div id="border-add">
-                {val.userID}
+                {val.id}
               </div>
               <div id="border-add">
-                {val.timeStamp}
+                {val.time}
               </div>
+              <br></br>
+              <div>
+                {
+                  val.adddisc.length>0 ? 
+               val.adddisc.map((index)=>{
+                    return (
+                      <>
+                      <div id="border-add">
+                      {index.text}
+                    </div>
+                    <div id="border-add">
+                      {index.time}
+                    </div>
+                      </>
+                    )
+                  }) : 
+                  <>
+                     No Extra Discussion
+                  </>
+                }
+              </div>
+              <button onClick={()=> {
+                handleClickOpen(val.id)
+              } }>Add Text</button>
             <br></br>
             <br></br>
               </center>
@@ -67,6 +116,29 @@ function DisPage() {
               </>
           })
         }
+        <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Title</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Add Text
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="text"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(event)=>{
+                  vals = event.target.value
+                  setTextBox(event.target.value)}}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
             <div id="login-basic">
               <label>Text :  </label><input type="text"
               onChange={(event)=>{
